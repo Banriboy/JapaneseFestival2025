6858f3bc9533386ee0107c7a908b60571450c381
Banri Ouchi <75915676+Banriboy@users.noreply.github.com>
2025-04-06 16:52:40 -0400

    Update jfTest.py

--- jfTest.py
+++ jfTest.py
@@ -62,7 +62,7 @@ st.title("🌎 Our Recycling Efforts Results")
 col1, col2 = st.columns(2)
 
 with col1:
-if "chopsticks" in category_totals:
+    f "chopsticks" in category_totals:
     # アニメーション用の空要素
     chopsticks_placeholder = st.empty()
 
@@ -93,7 +93,7 @@ if "chopsticks" in category_totals:
         """, unsafe_allow_html=True)
 
         time.sleep(0.03)  # スピード調整（数字が速く/遅く増える）
-
+        
 with col2:
     if "recycle" in category_totals:
         st.markdown(f"""
@@ -125,4 +125,3 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 st.write("Thank you for your cooperation!")
-
